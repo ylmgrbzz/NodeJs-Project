f4a1ebdf4cb46ac59f687e0043a8e45ae571a0dc
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-03-26 11:21:46 +0300

    NodeJs Project

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import users from './routes/users.js';
+
 const app = express();
 const port = 3000
+
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.send('Hello Worrldddd!');
